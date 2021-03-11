b5fbd7532896cc2a41192c98ff8dc3c2dc13f49e
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2021-03-11 09:36:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -55,29 +55,35 @@ int main(int argc, char const *argv[]){
     */
    
     
+    double parsec =1E16; // Reférence pour la longueur
+    double m_ref =1E36; // Référence pour la masse 
+    double ref_temps = 1E4; //Référence unité de temps 
+    double ref_vit = parsec/ref_temps ;
+
     /*
     ofstream fichier("resultats.txt", ios::out| ios::trunc);
     if (fichier){
-        Particule P1(1,0, 0, 0, 0);
-        Particule P2(1000000000000,80, 0, 0, 0,0);
-        Particule P3(100,90,0,0,0,0,0);
+        Particule P1(1E-6,0, 0, 0, 0);
+        Particule P2(1E-12,1E-9, 0, 0, 0,1E7/ref_vit);
+        //Particule P3(1./3,90,0,0,0,0,0);
         list<Particule> particules;
         particules.push_back(P1);
         particules.push_back(P2);
-        particules.push_back(P3);
+        //particules.push_back(P3);
         Boite primal;
         primal = first_box(particules);
         create_graph(&primal, particules);
         //print_graph(&primal);
         all_forces(&primal, &primal);
+        global_initialisation(particules);
         //On fait évoluer le système sur 10 pas de temps
-        int temps=3000;
+        int temps=3000000;
         int step=0; 
         list<Particule>::iterator it =particules.begin();
         for(;it!=particules.end();it ++){
-            fichier<< it->r_x <<"\t"<< it->r_y << "\t"<< it->r_z<<"\t"<<it->v_x<<"\t"<<it->v_y<<"\t"<<it->v_z<<"\n"<<endl;
+            fichier<< parsec*it->r_x <<"\t"<< parsec*it->r_y << "\t"<< parsec*it->r_z<<"\t"<<it->v_x<<"\t"<<it->v_y<<"\t"<<it->v_z<<'\t'<<it->F_x<<'\t'<<it->F_y<<"\n"<<endl;
         }
-        global_initialisation(particules);
+        
         for(step=0;step<=temps;step++){
             all_forces(&primal, &primal);
             global_update(particules);
@@ -85,7 +91,7 @@ int main(int argc, char const *argv[]){
             //Ecriture dans le fichier
             list<Particule>::iterator it =particules.begin();
             for(;it!=particules.end();it ++){
-                fichier<< it->r_x <<"\t"<< it->r_y << "\t"<< it->r_z<<"\t"<<it->v_x<<"\t"<<it->v_y<<"\t"<<it->v_z<<"\n"<<endl;
+                fichier<< parsec*it->r_x <<"\t"<< parsec*it->r_y << "\t"<< parsec*it->r_z<<"\t"<<ref_vit*it->v_x<<"\t"<<ref_vit*it->v_y<<"\t"<<ref_vit*it->v_z<<'\t'<<it->F_x<<'\t'<<it->F_y<<"\n"<<endl;
             }
             create_graph(&primal, particules);
         }
