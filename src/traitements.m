766666fa56c14280a2c4603233c0efadf2af13ff
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2021-03-17 17:57:33 +0100

    Update traitements.m

--- src/traitements.m
+++ src/traitements.m
@@ -1,7 +1,7 @@
 %%
 load('resultats.txt'); 
 [N,p]=size(resultats);
-
+load('energie.txt');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% N_steps correspond au nombre d'it?rations + 2 %%%%%%%%%%%
@@ -24,29 +24,40 @@ for i=1:Nb_particules
     end
 end
 fprintf('Done\n');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Evolution de l'energie mecanique %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+temps =1:N_steps;
+plot(temps,energie);
+xlabel("Iteration")
+ylabel('Energie')
+pause(10)
 
-
-%%
-for j=2:N_steps
-    for i=1:Nb_particules
-       plot(dynamique_x(i,j),dynamique_y(i,j),'k.','MarkerSize',10);
-       
-       
-       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-       %%% Adapter la fen?tre de visualisation pour coller avec votre
-       %%% syst?me
-       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-       
-       
-       xlim([-2*1e2; 2*1e2])
-       ylim([-2*1e2; 2*1e2])
-       hold on;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%Affichage et conception d'une video%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+vidfile = VideoWriter('3body.mp4','MPEG-4');
+open(vidfile);
+for j=2:N_steps+1
+    if mod(j,20)==0 
+        for i=1:Nb_particules
+            plot(dynamique_x(i,j),dynamique_y(i,j),'k.','MarkerSize',10);
+             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+             %%% Adapter la fen?tre de visualisation pour coller avec votre
+            %%% syst?me
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            xlim([-3; 3])
+            ylim([-3; 3])
+            hold on;
+        end
+        drawnow;  hold off;
+        F = getframe(gcf);
+        writeVideo(vidfile, F);
+        pause(0.01);
     end
-    drawnow;  
-    hold off; 
-    j
-    pause(0.1);
 end
+close(vidfile)
+
 
 fprintf('Done_2\n');
 
